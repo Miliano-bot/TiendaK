ce9efaedc781cc5e65900d5beb82e0df4a44fd99
Miliano <maxim@MILIANO>
2026-03-30 13:50:49 -0500

    primeros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,7 @@
-import { useEffect, useState } from 'react'
-import { supabase } from './supabaseClient' // asegúrate de que el path sea correcto
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  const [productos, setProductos] = useState([])
-
-  useEffect(() => {
-    cargarProductos()
-  }, [])
-
-  const cargarProductos = async () => {
-    const { data, error } = await supabase
-      .from('Productos')           // mayúscula inicial
-      .select('*')
-      .eq('discontinuo', false)    // solo productos activos
-
-    if (error) {
-      console.error('Error cargando productos:', error)
-    } else {
-      setProductos(data)
-    }
-  }
-
-  return (
-    <div style={{padding:'20px'}}>
-      <h1>Productos de prueba</h1>
-
-      {productos.length === 0 ? (
-        <p>No hay productos</p>
-      ) : (
-        productos.map(p => (
-          <div 
-            key={p.IdProducto} 
-            style={{border:'1px solid gray', margin:'5px', padding:'10px', borderRadius:'5px'}}
-          >
-            <h2>{p.nombre}</h2>
-            <p>Precio: ${p.precio}</p>
-            <p>Cantidad: {p.cantidad}</p>
-          </div>
-        ))
-      )}
-    </div>
-  )
+  return <Dashboard />;
 }
 
-export default App
+export default App;
